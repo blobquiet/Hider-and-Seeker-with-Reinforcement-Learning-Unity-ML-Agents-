efb0b8fbcb39e4e5e4bf96fbbe2e46972f2a2a8a
David <soquiet@hotmail.com>
2021-10-22 14:19:54 +0200

    creating a basic example

---
+++ Assets/Basic Example/Scripts/MoveToGoalAgent.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer FloorMeshrender;
+    
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3 (Random.Range(-4,+4),0,Random.Range(-2,+4));
+        targetTransform.localPosition = new Vector3 (Random.Range(-5,+5),0,Random.Range(-3,+5));
+    }
+
+    
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Data AI needs to solve the problem
+        // Moving the player (player position, and target position)
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 5f;
+        transform.localPosition += new Vector3(moveX,0,moveZ)* Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.name == "PlatformBorder"){
+            FloorMeshrender.material = loseMaterial;
+            SetReward(-1f);
+            EndEpisode();
+        }
+        if (other.name =="LargeGoal"){
+            FloorMeshrender.material = winMaterial;
+            SetReward(1f);
+            EndEpisode();
+        }
+        
+    }
+
+
+
+}
